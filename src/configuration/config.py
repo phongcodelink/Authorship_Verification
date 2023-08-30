52080e7b941b4bb45b3337809d1606530cb8b577
Phong Nguyen <ntranphong@gmail.com>
2023-08-30 08:48:53 +0700

    resize data

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -75,7 +75,7 @@ class BaseConfig:
                                              2].__str__() + "/assets/saved_models/"),
 
         self.parser.add_argument("--language_model_path", type=str,
-                                 default="google/flan-t5-base",
+                                 default="google/flan-t5-large",
                                  help="Path of the multilingual lm model dir")
 
         self.parser.add_argument("--csv_logger_path", type=str,
